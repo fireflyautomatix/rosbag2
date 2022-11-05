f8ecb8769541584c5b102fdf013bc74eabe1205a
rshanor <rickshanor@gmail.com>
2022-11-05 14:06:37 -0700

    Add pause and resume service calls for rosbag2 recorder (#1131)
    * feat(recorder): Add pause and resume service calls.
    
    Add service calls to pause recording and resume recording.
    
    Fixes https://github.com/ros2/rosbag2/issues/1130
    
    Signed-off-by: Rick Shanor <rickshanor@gmail.com>
    
    * feat(recorder): pause, resume, and is_paused PR comments.
    
    Address PR comments. Add is_paused service. Update tests accordingly.
    
    Signed-off-by: Rick Shanor <rickshanor@gmail.com>
    
    Signed-off-by: Rick Shanor <rickshanor@gmail.com>

--- rosbag2_transport/test/rosbag2_transport/test_record_services.cpp
+++ rosbag2_transport/test/rosbag2_transport/test_record_services.cpp
@@ -21,6 +21,9 @@
 
 #include "rclcpp/rclcpp.hpp"
 
+#include "rosbag2_interfaces/srv/is_paused.hpp"
+#include "rosbag2_interfaces/srv/pause.hpp"
+#include "rosbag2_interfaces/srv/resume.hpp"
 #include "rosbag2_interfaces/srv/snapshot.hpp"
 #include "rosbag2_transport/recorder.hpp"
 
@@ -35,6 +38,9 @@ using namespace ::testing;  // NOLINT
 class RecordSrvsTest : public RecordIntegrationTestFixture
 {
 public:
+  using IsPaused = rosbag2_interfaces::srv::IsPaused;
+  using Pause = rosbag2_interfaces::srv::Pause;
+  using Resume = rosbag2_interfaces::srv::Resume;
   using Snapshot = rosbag2_interfaces::srv::Snapshot;
 
   RecordSrvsTest()
@@ -71,8 +77,10 @@ public:
     pub_manager.setup_publisher(test_topic_, string_message, 10);
 
     const std::string ns = "/" + recorder_name_;
+    cli_is_paused_ = client_node_->create_client<IsPaused>(ns + "/is_paused");
+    cli_pause_ = client_node_->create_client<Pause>(ns + "/pause");
+    cli_resume_ = client_node_->create_client<Resume>(ns + "/resume");
     cli_snapshot_ = client_node_->create_client<Snapshot>(ns + "/snapshot");
-
     exec_ = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
 
     exec_->add_node(recorder_);
@@ -126,6 +134,9 @@ public:
 
   // Service clients
   rclcpp::Node::SharedPtr client_node_;
+  rclcpp::Client<IsPaused>::SharedPtr cli_is_paused_;
+  rclcpp::Client<Pause>::SharedPtr cli_pause_;
+  rclcpp::Client<Resume>::SharedPtr cli_resume_;
   rclcpp::Client<Snapshot>::SharedPtr cli_snapshot_;
 };
 
